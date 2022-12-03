ac5bb945bf276b38d4a8efafb7db77deb22785be
ronhaland <ronnyhaland@gmail.com>
2022-12-03 13:26:07 +0100

    Day 3 part 1 ⭐

---
+++ Day3/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine(ConvertToPrio('p')); //97
+Console.WriteLine(ConvertToPrio('P')); //65
+Console.WriteLine(ConvertToPrio('Z'));
+
+
+var backpacksInput = await File.ReadAllLinesAsync("Input.txt");
+var backpacksWithTwoCompartments = backpacksInput.Select(b => new { one = b[..(b.Length / 2)].Distinct(), two = b[(b.Length / 2)..].Distinct() });
+
+List<char> duplicateItems = new();
+
+foreach (var backpackWithTwoCompartments in backpacksWithTwoCompartments)
+{
+    foreach (var item in backpackWithTwoCompartments.one)
+    {
+        if (backpackWithTwoCompartments.two.Contains(item))
+            duplicateItems.Add(item);
+    }
+}
+
+
+Console.WriteLine("Sum of priorities for duplicates is:");
+Console.WriteLine(duplicateItems.Sum(ConvertToPrio));
+
+static int ConvertToPrio(char character)
+{
+    if (character >= 97)
+        return character - 96;
+    return character - 65 + 27;
+}
