06dc10bad0c0f148c49327143eb5c052f637475c
RonHaland <ronny.haland@gmail.com>
2022-12-04 13:44:17 +0100

    Day4 part 1 ⭐

---
+++ Day4/Program.cs
@@ -0,0 +1,32 @@
+﻿var pairs = (await File.ReadAllLinesAsync("Input.txt")).Select(p => new Pair(p)).ToList();
+
+int count = 0;
+foreach (var pair in pairs)
+{
+    if (pair.Range1.Item1 <= pair.Range2.Item1 && pair.Range1.Item2 >= pair.Range2.Item2)
+    {
+        count++;
+        continue;
+    }
+    if (pair.Range2.Item1 <= pair.Range1.Item1 && pair.Range2.Item2 >= pair.Range1.Item2)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("The count of fully contained ranges within the pairs is:");
+Console.WriteLine(count);
+
+internal sealed class Pair
+{
+	public Pair(string input)
+	{
+		var r1 = input.Split(",").First();
+        var r2 = input.Split(",").Last();
+		Range1 = new(int.Parse(r1.Split('-').First()), int.Parse(r1.Split('-').Last()));
+        Range2 = new(int.Parse(r2.Split('-').First()), int.Parse(r2.Split('-').Last()));
+    }
+
+	public Tuple<int, int> Range1 { get; }
+    public Tuple<int, int> Range2 { get; }
+}
