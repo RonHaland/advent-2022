89971f1099434db4c82420dc8b8d643b450a6121
RonHaland <ronny.haland@gmail.com>
2022-12-06 10:09:52 +0100

    Day 6 part 1 ⭐

---
+++ Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿var signal = (await File.ReadAllTextAsync("Input.txt")).ToQueue();
+//var signal = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".ToQueue();
+
+Queue<char> buffer = new();
+while (buffer.Count < 4)
+{
+    buffer.Enqueue(signal.Dequeue());
+}
+
+int signalStart = 4;
+while(signal.Count > 0)
+{
+    if (buffer.Count == buffer.Distinct().Count())
+    {
+        Console.WriteLine("Done with loop {0}", signalStart);
+        break;
+    }
+    buffer.Dequeue();
+    buffer.Enqueue(signal.Dequeue());
+    signalStart++;
+}
+
+internal static class StringExt
+{
+    internal static Queue<char> ToQueue(this string input)
+    {
+        Queue<char> queue = new();
+        foreach (char c in input)
+        {
+            queue.Enqueue(c);
+        }
+        return queue;
+    }
+}
