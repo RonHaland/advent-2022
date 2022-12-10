63dcb739abef03a3d530eb3e38d4e6e36fb3b053
RonHaland <ronny.haland@gmail.com>
2022-12-10 11:03:36 +0100

    part 2 ⭐

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -3,20 +3,22 @@
 var cycle = 1;
 var X = 1;
 List<int> signals = new();
+var lines = Enumerable.Range(0, 6).Select(l => "").ToList();
 
 for (int i = 0; i < input.Length; i++)
 {
-    cycle++;
+    IncrementCycle(X);
     SignalStrength(X, cycle, signals);
     if (input[i] == "noop")
         continue;
 
-    cycle++;
+    IncrementCycle(X);
     X += int.Parse(input[i].Split(' ')[1]);
     SignalStrength(X, cycle, signals);
 }
 
 Console.WriteLine("{0}",signals.Sum());
+Console.WriteLine(string.Join(Environment.NewLine, lines));
 
 void SignalStrength(int reg, int cycle, List<int> signals)
 {
@@ -25,4 +27,14 @@ void SignalStrength(int reg, int cycle, List<int> signals)
         signals.Add(reg * cycle);
         Console.WriteLine("{0}: {1}", cycle, reg * cycle);
     }
+}
+
+void IncrementCycle(int reg)
+{
+    if (cycle % 40 >= reg && cycle % 40 <= reg + 2)
+        lines[(cycle-1) / 40] += "#";
+    else
+        lines[(cycle-1) / 40] += ".";
+
+    cycle++;
 }
